1ebfc61f3a9701797dc6315326e6f5e56f362262
Thomas Chamberlin <tchamber@nrao.edu>
2024-06-10 16:53:56 -0400

    Rewrite example file download funcs

--- src/dysh/util/data.py
+++ src/dysh/util/data.py
@@ -1,78 +1,49 @@
-""" """
+from urllib.parse import urljoin
 
-import os
-import tarfile
-from pathlib import Path
+from astropy.utils.data import download_file as astropy_download_file
 
-import wget
+# TODO: Replace with a value from config file
+DYSH_URL = "https://www.gb.nrao.edu/dysh/example_data/"
 
 
-def get_example_data(example, destination=None, rootname="dysh"):
-    """ """
+def download_file(*args, **kwargs):
+    kwargs.setdefault("pkgname", "dysh")
+    kwargs.setdefault("cache", True)
+    return astropy_download_file(*args, **kwargs)
 
-    dysh_url = "https://www.gb.nrao.edu/dysh/example_data/"
+
+def get_example_data(example: str | None = None, **kwargs):
+    """Retrieve example data
+
+    If `example` is None, all example data will be downloaded. Otherwise, specifying it
+    will allow you to download a specific example dataset
+
+    All `kwargs` are passed through to astropy.utils.data.download_file"""
 
     examples = {
-        "positionswitch": {
-            "url": f"{dysh_url}/positionswitch/data/AGBT05B_047_01/AGBT05B_047_01.raw.acs/AGBT05B_047_01.raw.acs.fits",
-            "action": None,
-        },
-        "frequencyswitch": {
-            "url": f"{dysh_url}/frequencyswitch/data/TREG_050627/TREG_050627.raw.acs/TREG_050627.raw.acs.fits",
-            "action": None,
-        },
-        "subbeamnod": {
-            "url": f"{dysh_url}/subbeamnod/data/AGBT13A_124_06/AGBT13A_124_06.raw.acs/AGBT13A_124_06.raw.acs.fits",
-            "action": None,
-        },
-        "multiplefiles": {"url": f"{dysh_url}/multifits/data/TRCO_230413_Ka.raw.vegas.tgz", "action": unpack},
+        "positionswitch": [
+            "positionswitch/data/AGBT05B_047_01/AGBT05B_047_01.raw.acs/AGBT05B_047_01.raw.acs.fits",
+        ],
+        "frequencyswitch": [
+            "frequencyswitch/data/TREG_050627/TREG_050627.raw.acs/TREG_050627.raw.acs.fits",
+        ],
+        "subbeamnod": [
+            "subbeamnod/data/AGBT13A_124_06/AGBT13A_124_06.raw.acs/AGBT13A_124_06.raw.acs.fits",
+        ],
+        "multiplefiles": [
+            "multifits/TRCO_230413_Ka.raw.vegas/TRCO_230413_Ka.raw.vegas.A.fits",
+            "multifits/TRCO_230413_Ka.raw.vegas/TRCO_230413_Ka.raw.vegas.B.fits",
+            "multifits/TRCO_230413_Ka.raw.vegas/TRCO_230413_Ka.raw.vegas.C.fits",
+            "multifits/TRCO_230413_Ka.raw.vegas/TRCO_230413_Ka.raw.vegas.D.fits",
+        ],
     }
 
-    if example not in examples.keys():
-        raise KeyError(f"{example} is not available. The following are: {', '.join(examples.keys())}")
-
-    # Handle destination dir.
-    if destination is None:
-        xch = os.environ.get("XDG_CACHE_HOME")
-        # Use `XDG_CACHE_HOME` if it exists and is set.
-        if xch is not None and os.path.exists(xch):
-            xchpth = os.path.join(xch, rootname)
-            # Check for dysh subdir.
-            if os.path.exists(xchpth):
-                dest = Path(xchpth)
-            # Make it if not there already.
-            else:
-                dest = Path(xchpth)
-                dest.mkdir()
-        # Use the current directory if `XDG_CACHE_HOME` is not set or does not exist.
-        else:
-            dest = Path(".")
-    # User provided destination.
-    else:
-        dest = Path(destination)
-
-    if os.path.exists(dest):
-        output = dest / examples[example]["url"].split("/")[-1]
-        # Check if file exsists.
-        if output.is_file():
-            filename = output
-        else:
-            print(f"Will download data from: {examples[example]['url']}")
-            filename = Path(wget.download(examples[example]["url"], out=str(output)))
-
-    if examples[example]["action"] is not None:
-        filename = examples[example]["action"](filename, filename.parent)
-
-    print("")
-    print(f"Data is available in : {filename}")
-
-    return filename
-
-
-def unpack(filename, dest):
-    """ """
-
-    tar = tarfile.open(filename)
-    tar.extractall(path=dest)
-
-    return dest.joinpath(filename.stem)
+    try:
+        return {
+            e: download_file(urljoin(DYSH_URL, url), **kwargs)
+            for e, urls in examples.items()
+            for url in urls
+            if e == example or example is None
+        }
+    except KeyError as error:
+        raise KeyError(f"{example} is not available. The following are: {', '.join(examples.keys())}") from error
